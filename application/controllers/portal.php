beae4d901f4b5e37cb357290d9ec4ee0721ffdd7
Umar Mukhtar <umar@tuffah.info>
2016-04-07 16:35:57 +0800

    htaccess ckit

--- application/controllers/portal.php
+++ application/controllers/portal.php
@@ -19,9 +19,9 @@ class Portal extends CI_Controller {
         }
         */
         echo '
-        <a href="'.base_url().'index.php/portal/post/add">Add Post</a><br>
-        <a href="'.base_url().'index.php/portal/post">Post List</a><br>
-        <a href="'.base_url().'index.php/portal/index/true">Post Member Only</a><br>';
+        <a href="'.site_url().'portal/post/add">Add Post</a><br>
+        <a href="'.site_url().'portal/post">Post List</a><br>
+        <a href="'.site_url().'portal/index/true">Post Member Only</a><br>';
 		$data['nav'] = $this->displayNav($private);
         $data['postList'] = $this->_displayPost(null,$private);//null utk display all
 		
@@ -115,8 +115,8 @@ class Portal extends CI_Controller {
     {
         $huhu = $this->model_post->getPostLimit($limit, $lastPost);
         echo '
-        <a href="'.base_url().'index.php/portal/post/add">Add Post</a><br>
-        <a href="'.base_url().'index.php/portal/post">Post List</a><br>';
+        <a href="'.site_url().'portal/post/add">Add Post</a><br>
+        <a href="'.site_url().'portal/post">Post List</a><br>';
         $data['nav'] = $this->displayNav();
         $data['postList'] = $this->_displayPost($huhu);//null utk display all
         
@@ -269,7 +269,7 @@ class Portal extends CI_Controller {
                 <time datetime="'.$y.'-'.$m.'-'.$d.'" class="post-date entry-date updated" itemprop="datePublished">
                     <span>'. $this->month($m) .' '.$d.', '.$y.'</span>
                 </time>
-                <h1 class="post-title entry-title" itemprop="name"><a href="'. base_url().'index.php/portal/postContent/'.$id.'/" title="'.$t.' " >'.$t.'</a></h1>
+                <h1 class="post-title entry-title" itemprop="name"><a href="'. site_url().'portal/postContent/'.$id.'/" title="'.$t.' " >'.$t.'</a></h1>
             <!-- / Title, post icon, post date -->
             <div class="post-content">  
             <div class="entry-content" itemprop="articleBody">  
@@ -322,7 +322,7 @@ class Portal extends CI_Controller {
     public function lineMonth($month = null , $year = null)
     {
         $line = '<li>
-                    <a class="jump-month" data-jump="jump-'.$year.'-'.$month.'" data-year="'.$year.'"" data-month="'.$month.'" href="'.base_url().'postline/'.$year.'/'.$month.'">
+                    <a class="jump-month" data-jump="jump-'.$year.'-'.$month.'" data-year="'.$year.'"" data-month="'.$month.'" href="'.site_url().'postline/'.$year.'/'.$month.'">
                     '.$this->month($month).'</a>
                 </li>
                 ';
